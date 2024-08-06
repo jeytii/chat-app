caf5b6263bb0932efde7ad97e07a457a6a9e95b2
Jonathan Tarnate <jstarnate16@gmail.com>
2024-08-06 22:20:41 +0800

    Refresh page after login and logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
 
 class AuthController extends Controller
 {
@@ -22,7 +23,7 @@ class AuthController extends Controller
         if (Auth::attempt($data, true)) {
             $request->session()->regenerate();
 
-            return to_route('index');
+            return Inertia::location(route('index'));
         }
 
         return back()->withErrors([
@@ -38,6 +39,6 @@ class AuthController extends Controller
 
         $request->session()->regenerate();
 
-        return to_route('login.page');
+        return Inertia::location(route('login.page'));
     }
 }
