2abe86ed7ed0a6b34446977e2df8223b4caee56e
Jonathan Tarnate <jstarnate16@gmail.com>
2024-08-16 21:48:17 +0800

    Add user search endpoint

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,11 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
+    public function search(Request $request)
+    {
+        $query = $request->get('query');
+
+        $users = User::query()
+            ->whereNot('id', Auth::id())
+            ->where(fn (Builder $builder) => (
+                $builder->where('first_name', 'like', "%{$query}%")
+                    ->orWhere('last_name', 'like', "%{$query}%")
+                    ->orWhere('username', 'like', "%{$query}%")
+            ))
+            ->get();
+
+        return compact('users');
+    }
+
     public function toggleDarkMode()
     {
         /** @var App\Models\User */
--- routes/web.php
+++ routes/web.php
@@ -6,18 +6,19 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->controller(AuthController::class)->group(function () {
-    Route::get('login', 'showLoginPage')->name('login.page');
+    Route::inertia('login', 'Login')->name('login.page');
     Route::post('login', 'login')->name('login');
 });
 
-Route::middleware('auth')->controller(ChatController::class)->group(function () {
-    Route::get('/', 'index')->name('index');
+Route::middleware('auth')->group(function () {
+    Route::controller(ChatController::class)->group(function () {
+        Route::get('/', 'index')->name('index');
+    });
+    
+    Route::controller(UserController::class)->group(function () {
+        Route::get('users/search', 'search')->name('users.search');
+        Route::put('toggle-dark-mode', 'toggleDarkMode')->name('toggle-dark-mode');
+    });
+    
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 });
-
-Route::put('toggle-dark-mode', [UserController::class, 'toggleDarkMode'])
-    ->middleware('auth')
-    ->name('toggle-dark-mode');
-
-Route::post('/logout', [AuthController::class, 'logout'])
-    ->middleware('auth')
-    ->name('logout');
