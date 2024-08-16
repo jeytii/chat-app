69dada02b1c1eb299ee10c5a71233f4e187eb8aa
Jonathan Tarnate <jstarnate16@gmail.com>
2024-08-16 21:47:46 +0800

    Create SearchBox component

--- resources/js/Pages/Index.tsx
+++ resources/js/Pages/Index.tsx
@@ -1,14 +1,34 @@
+import { useMemo } from 'react'
+import { type PageProps } from '@inertiajs/core'
+import { usePage } from '@inertiajs/react'
+import { useQuery } from '@tanstack/react-query'
 import { ChevronDown, UserMinus } from 'lucide-react'
 import SettingsMenu from '@/components/SettingsMenu'
 import AvatarWithInfo from '@/components/AvatarWithInfo'
 import MessageBox from '@/components/MessageBox'
+import SearchBox from '@/components/SearchBox'
 import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
 import { Card, CardContent } from '@/components/ui/card'
 import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog'
 
+interface User {
+  first_name: string;
+  last_name: string;
+  name: string;
+  username: string;
+  profile_photo?: string;
+  dark_mode: boolean;
+};
+
 export default function Index() {
+  const { users } = usePage<PageProps & { users: User[]; }>().props
+  const { data } = useQuery<User[]>({ queryKey: ['search-results'] })
+  const people = useMemo<User[]>(
+    () => data?.length ? data : users,
+    [data]
+  )
+
   return (
     <main className='flex'>
       <aside className='w-80'>
@@ -31,26 +51,33 @@ export default function Index() {
                 </PopoverContent>
               </Popover>
             </div>
-            <div className='mt-4'>
-              <Input
-                type='text'
-                placeholder='Search'
-              />
-            </div>
+
+            <SearchBox />
           </div>
-          <div className='flex-1 overflow-y-auto'>
-            <div className='flex items-center p-4'>
-              <AvatarWithInfo
-                name='John Doe'
-                url='https://github.com/shadcn.png'
-                secondaryText='johndoe'
-                isOnline
-              />
-              <span className='w-[25px] h-[25px] inline-flex items-center justify-center bg-primary text-primary-foreground text-xs rounded-full ml-auto'>
-                2
-              </span>
+          {people.length ? (
+            <div className='flex-1 overflow-y-auto'>
+              {people.map(user => (
+                <div
+                  key={user.username}
+                  className='flex items-center p-4'
+                >
+                  <AvatarWithInfo
+                    name={user.name}
+                    url={user.profile_photo}
+                    secondaryText={user.username}
+                    isOnline
+                  />
+                  <span className='w-[25px] h-[25px] inline-flex items-center justify-center bg-primary text-primary-foreground text-xs rounded-full ml-auto'>
+                  2
+                  </span>
+                </div>
+              ))}
             </div>
-          </div>
+          ) : (
+            <p className='text-gray-500 text-center px-4 mt-4'>
+              You haven&apos;t added anyone to your contacts list.
+            </p>
+          )}
         </div>
       </aside>
       <div className='flex-1 h-screen flex flex-col'>
---
+++ resources/js/components/SearchBox.tsx
@@ -0,0 +1,48 @@
+import { type ChangeEvent, useEffect, useRef, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { Input } from './ui/input';
+
+export default function SearchBox() {
+  const [search, setSearch] = useState<string>('')
+  const searchTimer = useRef<number>()
+  const { refetch } = useQuery({
+    queryKey: ['search-results'],
+    async queryFn() {
+      if (! search.length) {
+        return []
+      }
+
+      const response = await fetch(`/users/search?query=${search}`)
+      const data = await response.json()
+
+      return data.users
+    }
+  })
+
+  function handleSearchQuery(event: ChangeEvent<HTMLInputElement>) {
+    setSearch(event.target.value)
+  }
+
+  useEffect(() => {
+    searchTimer.current = setTimeout(() => {
+      refetch()
+    }, 1000)
+
+    return () => {
+      if (searchTimer.current) {
+        clearTimeout(searchTimer.current)
+      }
+    }
+  }, [search])
+
+  return (
+    <div className='mt-4'>
+      <Input
+        type='text'
+        placeholder='Search'
+        value={search}
+        onChange={handleSearchQuery}
+      />
+    </div>
+  )
+}
