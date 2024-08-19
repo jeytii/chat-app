ebb9fd661d0a4961069316f456362a8c3f23465e
Jonathan Tarnate <jstarnate16@gmail.com>
2024-08-19 22:19:15 +0800

    Populate user's existing conversations

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -2,14 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Conversation;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ChatController extends Controller
 {
     public function index()
     {
-        return inertia('Index', [
-            'users' => [],
-        ]);
+        $authId = Auth::id();
+        $users = Conversation::query()
+            ->where('inviter_id', $authId)
+            ->orWhere('invited_id', $authId)
+            ->get()
+            ->map(fn (Conversation $conversation) => (
+                $conversation->inviter_id !== $authId ? $conversation->inviter : $conversation->invited
+            ));
+
+        return inertia('Index', compact('users'));
     }
 }
