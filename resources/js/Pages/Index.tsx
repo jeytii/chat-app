3e9e71d266a4002670ad3d8eeee8d4d33caae508
Jonathan Tarnate <jstarnate16@gmail.com>
2024-08-24 21:44:11 +0800

    Conditionally render Chat panel

--- resources/js/Pages/Index.tsx
+++ resources/js/Pages/Index.tsx
@@ -1,9 +1,12 @@
+import { useMemo, useState } from 'react'
 import { type PageProps } from '@inertiajs/core'
-import { Link, usePage } from '@inertiajs/react'
+import { usePage } from '@inertiajs/react'
 import AvatarWithInfo from '@/components/AvatarWithInfo'
 import SettingsMenu from '@/components/SettingsMenu'
 import SearchBox from '@/components/SearchBox'
 import UsersList from '@/components/UsersList'
+import Chat from '@/components/Chat'
+import { Button } from '@/components/ui/button'
 import { type User } from '@/types'
 
 interface Props extends PageProps {
@@ -13,6 +16,11 @@ interface Props extends PageProps {
 
 export default function Index() {
   const { user, users } = usePage<Props>().props
+  const [currentUsername, setCurrentUsername] = useState<string | null>(null)
+  const currentUser = useMemo(
+    () => currentUsername ? users.find(user => user.username === currentUsername) : null,
+    [currentUsername]
+  )
 
   return (
     <main className='flex'>
@@ -29,10 +37,11 @@ export default function Index() {
           {users.length ? (
             <div className='flex-1 overflow-y-auto'>
               {users.map(user => (
-                <Link
+                <Button
                   key={user.username}
-                  className='flex items-center p-4 hover:bg-secondary'
-                  href='/'
+                  className='w-full h-auto flex items-center text-left p-4 hover:bg-secondary'
+                  variant='ghost'
+                  onClick={setCurrentUsername.bind(null, user.username)}
                 >
                   <AvatarWithInfo
                     name={user.name}
@@ -43,7 +52,7 @@ export default function Index() {
                   <span className='w-[25px] h-[25px] inline-flex items-center justify-center bg-primary text-primary-foreground text-xs rounded-full ml-auto'>
                     2
                   </span>
-                </Link>
+                </Button>
               ))}
             </div>
           ) : (
@@ -54,12 +63,19 @@ export default function Index() {
         </div>
       </aside>
 
-      <section className='flex-1 px-4'>
-        <div>
-          <SearchBox />
-          <UsersList />
-        </div>
-      </section>
+      {currentUser ? (
+        <Chat
+          user={currentUser}
+          close={setCurrentUsername.bind(null, null)}
+        />
+      ) : (
+        <section className='flex-1 px-4'>
+          <div>
+            <SearchBox />
+            <UsersList />
+          </div>
+        </section>
+      )}
     </main>
   )
 }
