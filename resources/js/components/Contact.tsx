9594fc8958a91c866145130dd6fd835c89c3640a
Jonathan Tarnate <jstarnate16@gmail.com>
2024-09-20 22:29:36 +0800

    Create Contact component

---
+++ resources/js/components/Contact.tsx
@@ -0,0 +1,39 @@
+import { useQueryClient } from '@tanstack/react-query'
+import Avatar from './Avatar'
+import { Button } from './ui/button'
+import { cn } from '@/lib/utils'
+import type { ChatContact } from '@/types'
+
+export default function Contact(props: ChatContact) {
+  const queryClient = useQueryClient()
+
+  function setCurrentContact() {
+    const currentUrl = new URL(window.location.href)
+
+    queryClient.setQueryData(['username'], props.username)
+
+    currentUrl.searchParams.set('username', props.username)
+    window.history.pushState({ username: props.username }, '', currentUrl)
+  }
+
+  return (
+    <Button
+      className={cn(
+        'flex h-auto w-full items-center rounded-none p-4 text-left hover:bg-secondary',
+        queryClient.getQueryData(['username']) === props.username && 'bg-secondary'
+      )}
+      variant='ghost'
+      onClick={setCurrentContact}
+    >
+      <Avatar
+        name={props.name}
+        url={props.profile_photo_url}
+        secondaryText={`@${props.username}`}
+        isOnline={props.is_online}
+      />
+      <span className='ml-auto inline-flex size-[25px] items-center justify-center rounded-full bg-primary text-xs text-primary-foreground'>
+        2
+      </span>
+    </Button>
+  )
+}
