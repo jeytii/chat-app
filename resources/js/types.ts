0403c2a238c9917659d3cedf74730f99cb804ad6
Jonathan Tarnate <jstarnate16@gmail.com>
2024-09-20 22:30:50 +0800

    Add ChatContact typehint

--- resources/js/types.ts
+++ resources/js/types.ts
@@ -1,10 +1,15 @@
 export interface User {
+  id: number;
   first_name: string;
   last_name: string;
   name: string;
   username: string;
   profile_photo_url?: string;
   dark_mode: boolean;
+}
+
+export interface ChatContact extends User {
+  is_online: boolean;
 }
 
 export interface Message {
