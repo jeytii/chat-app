42bab2f3abad3c5c0dc54ad8dd8931fc585596c6
Jonathan Tarnate <jstarnate16@gmail.com>
2024-09-20 22:30:19 +0800

    Add real-time contact presence indicator

--- resources/js/components/Sidebar.tsx
+++ resources/js/components/Sidebar.tsx
@@ -1,29 +1,106 @@
+import { useEffect } from 'react'
+import { useQuery, useQueryClient } from '@tanstack/react-query'
 import { usePage } from '@inertiajs/react'
-import Avatar from './Avatar'
-import Contacts from './Contacts'
 import UserSettings from './UserSettings'
+import Avatar from './Avatar'
+import Contact from './Contact'
 import type { PageProps } from '@inertiajs/core'
-import type { User } from '@/types'
+import type { ChatContact, User } from '@/types'
 
 interface Props extends PageProps {
   user: User;
+  contacts: ChatContact[];
 }
 
 export default function Sidebar() {
-  const { user } = usePage<Props>().props
+  const { user: authUser, contacts } = usePage<Props>().props
+  const queryClient = useQueryClient()
+  const { data, isSuccess } = useQuery<ChatContact[]>({
+    queryKey: ['contacts'],
+    initialData: contacts,
+    enabled: false,
+  })
+
+  useEffect(() => {
+    setTimeout(() => {
+      window.Echo.join('chat')
+        .here((users: User[]) => {
+          const onlineUsers = users.reduce((ids: number[], user) => {
+            if (user.id !== authUser.id) {
+              ids.push(user.id)
+            }
+  
+            return ids
+          }, [])
+  
+          queryClient.setQueryData<ChatContact[]>(
+            ['contacts'],
+            (prev) => prev?.map(contact => ({
+              ...contact,
+              is_online: onlineUsers.indexOf(contact.id) !== -1,
+            }))
+          )
+        })
+        .joining((joiningUser: User) => {
+          queryClient.setQueryData<ChatContact[]>(
+            ['contacts'],
+            (prev) => prev?.map(contact => {
+              if (contact.id === joiningUser.id) {
+                return {
+                  ...contact,
+                  is_online: true,
+                }
+              }
+  
+              return contact
+            })
+          )
+        })
+        .leaving((leavingUser: User) => {
+          queryClient.setQueryData<ChatContact[]>(
+            ['contacts'],
+            (prev) => prev?.map(contact => {
+              if (contact.id === leavingUser.id) {
+                return {
+                  ...contact,
+                  is_online: false,
+                }
+              }
+  
+              return contact
+            })
+          )
+        })
+    }, 1000)
+  }, [])
 
   return (
     <div className='flex h-full flex-col'>
       <div className='border-b border-border p-4'>
         <Avatar
-          name={user.name}
-          url={user.profile_photo_url}
+          name={authUser.name}
+          url={authUser.profile_photo_url}
         />
 
         <UserSettings />
       </div>
 
-      <Contacts />
+      {(isSuccess && !data.length) && (
+        <p className='mt-4 px-4 text-center text-gray-500'>
+          You haven&apos;t added anyone to your contacts list.
+        </p>
+      )}
+
+      {(isSuccess && data.length) && (
+        <div className='flex-1 overflow-y-auto'>
+          {data.map(contact => (
+            <Contact
+              key={contact.username}
+              { ...contact }
+            />
+          ))}
+        </div>
+      )}
     </div>
   )
 }
