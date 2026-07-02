262c74ace7ad08ea8707917771b03c0836e2f552
Jonathan Tarnate <jstarnate16@gmail.com>
2026-07-02 23:44:34 +0800

    Fix SidebarInset layout glitch

--- resources/js/layouts/app-layout.tsx
+++ resources/js/layouts/app-layout.tsx
@@ -2,15 +2,21 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { AppSidebar } from '@/components/app-sidebar'
 import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar'
+import { useCurrentUrl } from '@/hooks/use-current-url'
+import { cn } from '@/lib/utils'
 
 const queryClient = new QueryClient()
 
 export default function AppLayout({ children }: { children: React.ReactNode; }) {
+    const { currentUrl } = useCurrentUrl()
+
     return (
         <QueryClientProvider client={queryClient}>
             <SidebarProvider>
                 <AppSidebar />
-                <SidebarInset>
+                <SidebarInset className={cn(
+                    currentUrl.startsWith('/conversations') ? 'h-[calc(100vh-16px)]' : 'min-h-svh',
+                )}>
                     {children}
                 </SidebarInset>
             </SidebarProvider>
