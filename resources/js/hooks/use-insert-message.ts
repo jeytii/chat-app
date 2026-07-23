35e7fa49ed87758ee0b7712271d5eaa951548041
Jonathan Tarnate <jstarnate16@gmail.com>
2026-07-23 19:02:32 +0800

    Create use-insert-message.ts

---
+++ resources/js/hooks/use-insert-message.ts
@@ -0,0 +1,51 @@
+import { type InfiniteData, useQueryClient } from '@tanstack/react-query'
+import { useCallback } from 'react'
+
+import type { Message, MessageResponse } from '@/types/models'
+
+export function useInsertMessage() {
+    const queryClient = useQueryClient()
+
+    return useCallback((conversationId: number, message: Message, notFromSelf: boolean = false) => {
+        queryClient.setQueryData<InfiniteData<MessageResponse>>(['messages', conversationId], current => {
+            if (!current) {
+                return current
+            }
+
+            // Insert new item into a new page if the latest one has reached the pagination count
+            if (current.pages[current.pages.length - 1].items.length >= 20) {
+                return {
+                    pageParams: [
+                        null,
+                        ...current.pageParams,
+                    ],
+                    pages: [
+                        ...current.pages,
+                        {
+                            items: [notFromSelf ? { ...message, from_self: false } : message],
+                            next_cursor: null,
+                        },
+                    ],
+                }
+            }
+
+            // Else, push it into the latest page
+            return {
+                ...current,
+                pages: current.pages.map((page, index, pages) => {
+                    if (index === pages.length - 1) {
+                        return {
+                            ...page,
+                            items: [
+                                ...page.items,
+                                notFromSelf ? { ...message, from_self: false } : message,
+                            ],
+                        }
+                    }
+
+                    return page
+                }),
+            }
+        })
+    }, [queryClient])
+}
