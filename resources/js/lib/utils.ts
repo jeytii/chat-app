e9ebe8d67b4f862555842b0c3068d96d374632a0
Jonathan Tarnate <jstarnate16@gmail.com>
2026-07-27 23:50:42 +0800

    Add utils

--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -1,7 +1,18 @@
 import type { InertiaLinkProps } from '@inertiajs/react'
 import type { ClassValue } from 'clsx'
 import { clsx } from 'clsx'
+import Echo from 'laravel-echo'
 import { twMerge } from 'tailwind-merge'
+
+export const echo = new Echo({
+    broadcaster: 'reverb',
+    key: import.meta.env.VITE_REVERB_APP_KEY,
+    wsHost: import.meta.env.VITE_REVERB_HOST,
+    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
+    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
+    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+    enabledTransports: ['ws', 'wss'],
+})
 
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs))
@@ -9,4 +20,17 @@ export function cn(...inputs: ClassValue[]) {
 
 export function toUrl(url: NonNullable<InertiaLinkProps['href']>): string {
     return typeof url === 'string' ? url : url.url
+}
+
+export function toReadableDate(
+    date: string | Date,
+    month: Intl.DateTimeFormatOptions['month'] = 'long',
+    day: Intl.DateTimeFormatOptions['day'] = '2-digit',
+    year: Intl.DateTimeFormatOptions['year'] = 'numeric',
+) {
+    const value = typeof date === 'string'
+        ? new Date(date)
+        : date
+
+    return value.toLocaleDateString('en-PH', { month, day, year })
 }
