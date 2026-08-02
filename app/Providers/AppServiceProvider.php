d234d4f5c75a5f89c65f2bb3e8af29a735ff81b3
Jonathan Tarnate <jstarnate16@gmail.com>
2026-08-02 10:59:38 +0800

    Utilize Laravel's new dev command

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Carbon\CarbonImmutable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\DevCommands;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
@@ -17,7 +18,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        DevCommands::except('logs');
     }
 
     /**
