81a0f12901306e69d5234d5cb2b73aa6c3b3ed56
Jonathan Tarnate <jstarnate16@gmail.com>
2026-08-03 19:05:14 +0800

    Use regular Echo instance for whispers

--- resources/js/components/message-box.tsx
+++ resources/js/components/message-box.tsx
@@ -1,5 +1,5 @@
 import { usePage } from '@inertiajs/react'
-import { useEcho, useSocketId } from '@laravel/echo-react'
+import { useSocketId } from '@laravel/echo-react'
 import { useMessageScroller } from '@shadcn/react/message-scroller'
 import type { InfiniteData } from '@tanstack/react-query'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
@@ -17,6 +17,7 @@ import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover
 import { useAppearance } from '@/hooks/use-appearance'
 import { useInsertMessage } from '@/hooks/use-insert-message'
 import { useThrottle } from '@/hooks/use-limit'
+import usePrivateChannel from '@/hooks/use-private-channel'
 import { toReadableDate } from '@/lib/utils'
 import type { Message, MessageResponse } from '@/types/models'
 
@@ -33,7 +34,7 @@ export default function MessageBox() {
     const previewImage = useRef<string | null>(null)
     const indicateTyping = useThrottle(1000)
     const socketId = useSocketId()
-    const { channel } = useEcho(`conversation.${conversationId}`, '', () => { })
+    const channel = usePrivateChannel(`conversation.${conversationId}`)
     const queryKey = ['messages', conversationId]
 
 
@@ -128,7 +129,7 @@ export default function MessageBox() {
         setMessage(event.target.value)
 
         indicateTyping(() => {
-            channel()?.whisper('typing', {
+            channel.whisper('typing', {
                 username: auth.user.username,
             })
         })
--- resources/js/components/presence-indicator.tsx
+++ resources/js/components/presence-indicator.tsx
@@ -1,47 +1,32 @@
 import { usePage } from '@inertiajs/react'
-import Echo from 'laravel-echo'
 import { useEffect, useState } from 'react'
 
 import { useDebounce } from '@/hooks/use-limit'
-
-const echo = new Echo({
-    broadcaster: 'reverb',
-    key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
-    enabledTransports: ['ws', 'wss'],
-})
+import usePrivateChannel from '@/hooks/use-private-channel'
 
 export default function PresenceIndicator({ conversationId, isOnline }: { conversationId: number; isOnline: boolean; }) {
     const { username } = usePage().props.auth.user
     const [isTyping, setIsTyping] = useState<boolean>(false)
     const { debouncedFn } = useDebounce(1000)
+    const channel = usePrivateChannel(`conversation.${conversationId}`)
 
     useEffect(() => {
-        echo.private(`conversation.${conversationId}`)
-            .listenForWhisper('typing', data => {
-                if (data.username !== username) {
-                    if (data.sent) {
-                        setIsTyping(false)
-                    } else {
-                        setIsTyping(true)
+        channel.listenForWhisper('typing', data => {
+            if (data.username !== username) {
+                setIsTyping(true)
 
-                        debouncedFn(() => {
-                            setIsTyping(false)
-                        })
-                    }
-                }
-            })
+                debouncedFn(() => {
+                    setIsTyping(false)
+                })
+            }
+        })
 
         return () => {
-            echo.private(`conversation.${conversationId}`)
-                .stopListeningForWhisper('typing')
+            channel.stopListeningForWhisper('typing')
 
             setIsTyping(false)
         }
-    }, [conversationId, username, debouncedFn])
+    }, [conversationId, username, channel, debouncedFn])
 
     if (isTyping) {
         return (
