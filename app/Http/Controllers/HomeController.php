5b11b8f09cc7474fe4adf8dd0dc8daa758781a03
Jonathan Tarnate <jstarnate16@gmail.com>
2026-09-09 21:39:37 +0800

    Minor improvements

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Inertia\Response;
 
@@ -9,10 +10,15 @@ class HomeController extends Controller
 {
     public function __invoke(Request $request): Response
     {
+        /** @var User */
+        $user = $request->user();
         $tab = $request->query('tab');
 
         return inertia('home', [
             'tab' => \in_array($tab, ['received-requests', 'sent-requests']) ? $tab : 'chats',
+            'chatsCount' => $user->chats()->count(),
+            'receivedRequestsCount' => $user->receivedRequests()->count(),
+            'sentRequestsCount' => $user->sentRequests()->count(),
         ]);
     }
 }
